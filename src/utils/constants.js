a63ce61aff9e55db3e7b95233c14dae6faf95308
Eric Company <ericcovi@hotmail.com>
2022-06-28 11:36:01 +0200

    Fix URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
-export const BASE_API_URL = 'http://localhost:5005'
+export const BASE_API_URL = process.env.NODE_ENV === "production" ? "https://the-oracle-app.herokuapp.com" :
+'http://localhost:5005'
