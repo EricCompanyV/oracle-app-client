37a707dceaa11e902c61bda5bd3611602de6d821
ickejohannes <ickejohannes@gmail.com>
2022-06-29 15:25:04 +0200

    Implement functionality for No Button on Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,19 @@
 import { Box, Button, Text } from "@mantine/core";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { ThumbDown, ThumbUp } from "tabler-icons-react";
 
 function Homepage() {
+  const [text, setText] = useState("Welcome to the first day of your life. Would you like to decide something today?");
+
+  const changeText = () => {
+    setText("You just did. Would you like to take another one?");
+  }
+
   return (
     <>
       <Text>
-        Welcome to the first day of your life. Would you like to decide
-        something today?
+        {text}
       </Text>
       <Box>
         <Button
@@ -37,6 +43,7 @@ function Homepage() {
         <Button
           component={Link}
           to={`/`}
+          onClick={changeText}
           leftIcon={<ThumbDown size={24} strokeWidth={2} color={"white"} />}
           styles={(theme) => ({
             root: {
